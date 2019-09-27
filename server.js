7da227405232ac9db92c786599502d321e99a513
Valeria Guzman <valeria.guzman0509@gmail.com>
2019-09-26 20:05:20 -0500

    Index modified

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 const port = 9000
 
-app.use(express.static("public"))
+router.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.use("/asset", router)
 
 app.listen (port, () => {
     console.log("El servidor esta escuchando correctamente en el puerto 9000...");
